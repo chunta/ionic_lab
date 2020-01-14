997f7d3dff70bd35920edb8cea2ca5d63e710e38
rex.chen <rex.chen@appier.com>
2020-01-14 15:21:50 +0800

    install aiqua-qgsdk

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QGSdkPlugin } from 'aiqua-qgsdk'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor() {
+
+    // -- sts --
+    console.log('This is Tab1Page constructor');
+    let v = QGSdkPlugin.echo({value:'1'});
+    for (var key in v) {
+      console.log("User " + v[key] + " is #" + key);
+      if (typeof v[key] === 'object') {
+        console.log('return value:', v[key].value);
+      }
+    }
+    // -- end --
+
+  }
 
 }
